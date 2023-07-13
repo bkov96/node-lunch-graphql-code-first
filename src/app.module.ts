29d401e5510f1dea7ef7e97479177d1aafe54744
bence.kovacs2 <bence.kovacs2@supercharge.io>
2023-07-13 16:12:05 +0200

    feat: add global prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppResolver } from './app.resolver';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
